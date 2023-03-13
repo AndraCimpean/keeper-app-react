dfec907754438a340403b28c334e015a69853db7
AndraCimpean <andra.cimpean94@gmail.com>
2023-03-13 23:36:54 +0200

    add project files

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FitbitIcon from "@mui/icons-material/Fitbit";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FitbitIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
